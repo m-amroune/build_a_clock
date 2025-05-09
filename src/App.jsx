3d68d2aff4356a5a4e1daf4d2a6fc515764f9888
Moustapha <amroune.moustapha@gmail.com>
2025-05-09 15:00:01 +0200

    add formatTime function to format time in minutes and seconds

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import BreakControls from './components/BreakControls'
 import SessionControls from './components/SessionControls'
 import Timer from './components/Timer'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
   const [breakLength, setBreakLength] = useState(5); // state duration of break
@@ -11,14 +11,23 @@ function App() {
   const [sessionLength, setSessionLength]  = useState(25) // initial session duration
   const [timeRemaining, setTimeRemaining] = useState(sessionLength * 60)
   const [timerLabel, setTimerLabel] = useState('Session'); // state timer (session or break)
-;
+  const intervalRef = useRef(null);
+  const audioRef = useRef(null);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+  
+
   return (
     <div className="App">
       <h1>25 + 5 Clock</h1>
       <BreakControls breakLength={breakLength} setBreakLength={setBreakLength}
 isRunning={isRunning}/>
       <SessionControls sessionLength={sessionLength} setSessionLength={setSessionLength}  setTimeRemaining={setTimeRemaining} />
-      <Timer timerLabel={timerLabel} timeRemaining={timeRemaining} />
+      <Timer timerLabel={timerLabel} timeRemaining={formatTime(timeRemaining)} />
     </div>
     
   )
